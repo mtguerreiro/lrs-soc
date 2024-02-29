e635748be542482dbd1f11e3c059c080b786a9b6
Norena Esteban <n_esteban@lrs.eit.uni-kl.de>
2024-02-29 09:19:27 +0100

    deleted unnecesary command file

--- app/itm3903c/itm3903cCommands.h
+++
@@ -1,9 +0,0 @@
-#include "stdint.h"
-
-const char * COMMAND_GETVERSION = "SYST:VERS?\r\n";
-const int32_t SIZE_COMMAND_GETVERSION = 13;
-
-const char * COMMAND_GETERROR = "SYST:ERR?\r\n";
-const int32_t SIZE_COMMAND_GETERROR = 12;
-
-
